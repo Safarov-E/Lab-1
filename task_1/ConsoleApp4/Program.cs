9ddf44d769c0b34905a799887a92edd6e7c8ae28
Safarov-E <44256964+Safarov-E@users.noreply.github.com>
2018-11-08 18:50:51 +0500

    add_1

---
+++ task_1/ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n: ");
+            double n;
+            while (!(double.TryParse(Console.ReadLine(), out n)) || n <= 0)
+            {
+                Console.Write("Нельзя вычислить. Введите n: ");
+            }
+            Console.Write("Введите m: ");
+            double m;
+            while (!(double.TryParse(Console.ReadLine(), out m)) || m <= 1)
+            {
+                Console.Write("Нельзя вычислить. Введите m: ");
+            }
+            double k = (m) / (--n);
+            n++;
+            Console.WriteLine($"m/--n++ = {k}, n = {n}, m={m}");
+            Console.ReadLine();
+        }
+    }
+}
