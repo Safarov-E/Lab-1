07e1cbfa965c61fe8d76fdc2fb7ccb17df63c1bb
Safarov-E <44256964+Safarov-E@users.noreply.github.com>
2019-02-01 15:50:25 +0500

    Add files via upload

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -15,6 +15,7 @@ namespace ConsoleApplication1
             Console.Write("1. Удалить элемент из массива. \n2. Добавить элемент в массив. \n3. Переставить элементы в массиве. \n4. Поиск элемента в массиве. \n5. Сортировка элементов в массиве. \n6. Выход. \n");
             do
             {
+                Console.WriteLine();
                 Console.Write("Введите номер пункта: ");
                 while (!(int.TryParse(Console.ReadLine(), out a)) || a <= 0 || a > 6) ;
 
@@ -43,78 +44,176 @@ namespace ConsoleApplication1
             } while (!flag);
         }
         static void Deletion()
-        {   int vod;
+        {
+            int n;
             do
             {
                 Console.Write("\nВедите количество элементов: ");
-            }while (!(int.TryParse(Console.ReadLine(), out vod)) || vod <= 0);
-            
-            Random ran = new Random();
-            int[] n = new int[vod];            
-            for (int i = 0; i < vod; i++)
+            } while (!(int.TryParse(Console.ReadLine(), out n)) || n <= 0);
+            Random rnd = new Random();
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
             {
-                n[i] = ran.Next(0, 100);
-                Console.WriteLine($"vod[{i}] = {n[i]}");
+                numbers[i] = rnd.Next(0, 100);
+                Console.WriteLine($"n[{i}] = {numbers[i]}");
             }
-
-            int vod2;
-            do
+            Console.Write("Введите номер удаляемого элемента: ");
+            int a;
+            while (!(int.TryParse(Console.ReadLine(), out a)) || a < 0 || a >= n)
             {
-                Console.Write("\nВведите количество удаляемых елементов: ");
+                Console.Write("Введите номер удаляемого элемента: ");
             }
-            while (!(int.TryParse(Console.ReadLine(), out vod2)) || vod2 < 0 || vod2 >= vod);
-            
-            vod = vod - 1;
-            int[] n2 = new int[vod];
-            for (int i = 0, j = 0; i < n.Length; i++, j++)
+            n = n - 1;
+            int[] arr = new int[n];
+            for (int i = 0, d = 0; i < numbers.Length; i++, d++)
             {
-                if (i == vod2)
+                if (i == a)
                 {
                     i++;
                 }
-                if(i > n.Length -1)
+                if (i > numbers.Length - 1)
                 {
                     break;
-
                 }
-                n2[j] = n[i];
-                 
+                arr[d] = numbers[i];
             }
-            for (int i = 0; i < n2.Length; i++)
-                Console.WriteLine($"n2[{i}] = {n2[i]}");
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            Console.WriteLine();
+            Console.Write("1. Удалить элемент из массива. \n2. Добавить элемент в массив. \n3. Переставить элементы в массиве. \n4. Поиск элемента в массиве. \n5. Сортировка элементов в массиве. \n6. Выход. \n");
 
         }
-         static void Adding()
+        static void Adding()
         {
-            int vod;
-            do{
-                Console.Write("\nВведите количестов элементов");
-            }while (!(int.TryParse(Console.ReadLine(), out vod)) || vod <= 0) ;
-            Random ran = new Random();
-            int[] n = new int[vod];
-            for(int i = 0; i < vod; i++)
-            {
-                n[i] = ran.Next(1, 100);
-                Console.WriteLine($"vod[{i}] = {n[i]}");
-            }
-            int vod2;
+            int n;
             do
+            {
+                Console.Write("\nВведите количестов элементов: ");
+            } while (!(int.TryParse(Console.ReadLine(), out n)) || n <= 0);
+            int[] numbers = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = rnd.Next(1, 100);
+                Console.WriteLine($"n[{i}] = {numbers[i]}");
+            }
+            Console.Write("Введите количество добавляемых элементов: ");
+            int a;
+            while (!(int.TryParse(Console.ReadLine(), out a)) || n <= 0)
             {
                 Console.Write("Введите количество добавляемых элементов: ");
             }
+            int b = n + a;
+            int[] arr = new int[b];
+            for (int i = 0; i < a; i++)
+            {
+                arr[i] = rnd.Next(1, 100);
+            }
+            for (int i = a, c = 0; i < arr.Length; i++)
+            {
+                arr[i] = numbers[c];
+                c++;
+            }
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine($"arr[{i}] = {arr[i]}"); ;
+            Console.WriteLine();
+            Console.Write("1. Удалить элемент из массива. \n2. Добавить элемент в массив. \n3. Переставить элементы в массиве. \n4. Поиск элемента в массиве. \n5. Сортировка элементов в массиве. \n6. Выход. \n");
 
         }
             static void Permutation()
             {
-                Console.Write("Ведите количество элементов: ");
-            }
-             static void First_even()
+                int n;
+                do
+                {
+                    Console.Write("\nВедите количество элементов: ");
+                } while (!(int.TryParse(Console.ReadLine(), out n)) || n <= 0);
+            Random rmd = new Random();
+            int[] numbers = new int[n];
+            for(int i = 0; i < n; i++)
             {
-                Console.Write("Ведите количество элементов: ");
+                numbers[i] = rmd.Next(-10, 10);
+                Console.WriteLine($"n[{i}] = {numbers[i]}");
             }
+            Console.WriteLine("Положительные элементы переставлены в начало массива, отрицательные - в конец");
+            int[] arr = new int[n];
+            for(int  i = 0, c = n -1, d = 0, e = 0; d < arr.Length; d++, e++)
+            {
+                if (numbers[e] < 0)
+                {
+                    arr[i] = numbers[e];
+                    i++;
+                }
+                else
+                {
+                    arr[c] = numbers[e];
+                    c--;
+                }
+            }
+            for(int i = 0; i < arr.Length; i++)
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            Console.WriteLine();
+            Console.Write("1. Удалить элемент из массива. \n2. Добавить элемент в массив. \n3. Переставить элементы в массиве. \n4. Поиск элемента в массиве. \n5. Сортировка элементов в массиве. \n6. Выход. \n");
+
+        }
+            static void First_even()
+            {
+            int n;
+            do
+            {
+                Console.Write("\nВедите количество элементов: ");
+            } while (!(int.TryParse(Console.ReadLine(), out n)) || n <= 0);
+            Random rnd = new Random();
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = rnd.Next(1, 100);
+                Console.WriteLine($"n[{i}] = {numbers[i]}");
+            }
+            var numbersList = numbers.ToList();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.WriteLine($"Первый четный элемент в массиве {numbers[i]}");
+                    break;
+                }
+            }
+        }
+
              static void Sorting()
             {
-                Console.Write("Ведите количество элементов: ");
+                int n;
+                do
+                {
+                    Console.Write("\nВедите количество элементов: ");
+                } while (!(int.TryParse(Console.ReadLine(), out n)) || n <= 0);
+
+            Random rmd = new Random();
+            int[] numbers = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                numbers[i] = rmd.Next(-10, 10);
+                Console.WriteLine($"n[{i}] = {numbers[i]}");
             }
+            int j;
+            for(int i = 1; i < n; i++)
+            {
+                for(j = n - 1; j >= i; j--)
+                {
+                    if (numbers[j] < numbers[j - 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j - 1];
+                        numbers[j - 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Отсортированный массив");
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine($"n[{i}] = {numbers[i]}");
+            Console.WriteLine();
+            Console.Write("1. Удалить элемент из массива. \n2. Добавить элемент в массив. \n3. Переставить элементы в массиве. \n4. Поиск элемента в массиве. \n5. Сортировка элементов в массиве. \n6. Выход. \n");
+
+        }
     }
 }
